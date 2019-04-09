b3060c4452ad38914822823c49a9af9fc00287c7
JAngel <43564263+jenaym@users.noreply.github.com>
2019-04-09 15:13:05 -0700

    Updates

---
+++ precache-manifest.0a74dd20e33db777d48a39d9aa4320b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a8ce0ada1ccd7861e82",
+    "url": "/React-Game/static/js/runtime~main.d8037eb9.js"
+  },
+  {
+    "revision": "d35ef0ff7c4bee4f12f6",
+    "url": "/React-Game/static/js/main.11f4afba.chunk.js"
+  },
+  {
+    "revision": "684630195934f059cae0",
+    "url": "/React-Game/static/js/2.45a2f2d7.chunk.js"
+  },
+  {
+    "revision": "d35ef0ff7c4bee4f12f6",
+    "url": "/React-Game/static/css/main.76c3f9d4.chunk.css"
+  },
+  {
+    "revision": "434dd5ea2fbee379574efd995fcc92ad",
+    "url": "/React-Game/index.html"
+  }
+];
